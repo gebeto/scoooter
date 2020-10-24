2d7288d2e53730f11d1448286e002a4a09fe86f7
gebeto <slavik.nychkalo@gmail.com>
2020-10-24 17:58:55 +0300

    Added kiwi authorization pipeline.

--- services/kiwi.py
+++ services/kiwi.py
@@ -1,56 +1,75 @@
 import requests
-
-"""
-REQ
-curl -v -X POST 'http://service.kiwimobility.com/user/sign-with-phone'
- -H 'Accept: application/json, text/plain, */*'
- -H 'Content-Length: 46'
- -H 'Content-Type: application/json;charset=utf-8'
- -H 'Accept-Language: en-us'
- -H 'Accept-Encoding: gzip, deflate'
- -H 'Cookie:'
-
-RES
-{"id": 11111}
+import json
+import re
 
 
-
-
-
-REQ
-curl -v -X POST 'http://service.kiwimobility.com/user/verify'
- -H 'Accept: application/json, text/plain, */*'
- -H 'Content-Length: 30'
- -H 'Content-Type: application/json;charset=utf-8'
- -H 'Accept-Language: en-us'
- -H 'Accept-Encoding: gzip, deflate'
- -H 'Cookie:'
-
-RES
-{"token": "TOKEN"}
-
-
-
-
-REQ
-curl -v -X GET 'http://service.kiwimobility.com/vehicle/search?latitude=49.844730516318464&longitude=24.007530360235208&zoneId=45'
- -H 'Accept-Language: en-us'
- -H 'Accept: application/json, text/plain, */*'
- -H 'Authorization: Bearer TOKEN'
- -H 'Accept-Encoding: gzip, deflate'
- -H 'Cookie:'
-"""
-
-url = "http://service.kiwimobility.com/vehicle/search?latitude=49.844730516318464&longitude=24.007530360235208&zoneId=45"
+url_base = "http://service.kiwimobility.com"
 token = "TOKEN"
 
 
-def available_scooters():
-    response = requests.get(url, headers={
-        "Accept-Language": "en-us",
-        "Accept": "application/json, text/plain, */*",
-        "Authorization": f"Bearer {token}",
-        "Accept-Encoding": "gzip, deflate",
-    })
-    data = response.json()
-    return data
+def request_sms(phone_number):
+	phone_number = re.sub("^3?8?0?", "", phone_number)
+	url_request_sms = f"{url_base}/user/sign-with-phone"
+	request = requests.post(
+		url_request_sms,
+		# headers=HEADERS,
+		data=json.dumps({
+			"phoneCountryCode": "380",
+			"phone": phone_number,
+		})
+	)
+	return request.json()
+
+
+def confirm_sms(id, code):
+	url_confirm_sms = f"{url_base}/user/verify"
+	request = requests.post(
+		url_confirm_sms,
+		# headers=HEADERS,
+		data=json.dumps({
+			"id": id,
+			"authCode": code,
+		})
+	)
+	return request.json()["token"]
+
+
+def login():
+    phone_number = input("Phone number: ")
+    data = request_sms(phone_number)
+    code = input("Code from SMS: ")
+    token = confirm_sms(data["id"], code)
+    return token
+
+
+def to_simple_shape(scooter):
+    return {
+        "type": "kiwi",
+        "id": scooter["id"],
+        "title": scooter["name"],
+        "battery": scooter["power"],
+        "location": {
+            "lat": scooter["coordinates"]["latitude"],
+            "lon": scooter["coordinates"]["longitude"],
+        },
+    }
+
+
+def available_scooters(token):
+	url_scooters = f"{url_base}/vehicle/search"
+	response = requests.get(
+		url_scooters,
+		params={
+			"latitude": 49.844730516318464,
+			"longitude": 24.007530360235208,
+			"zoneId": 45,
+		},
+		headers={
+			"Accept-Language": "en-us",
+			"Accept": "application/json, text/plain, */*",
+			"Authorization": f"Bearer {token}",
+			"Accept-Encoding": "gzip, deflate",
+		}
+	)
+	data = response.json()
+	return [to_simple_shape(d) for d in data]
