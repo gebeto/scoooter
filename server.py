d76c5aa9542062f1f6906094bd1c21d34c08e6ad
gebeto <slavik.nychkalo@gmail.com>
2020-10-24 20:19:08 +0300

    Added markers for ewings and kiwi

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ from main import get_all_scooters
 async def index(request):
 	return web.FileResponse("html/index.html")
 
+async def marker(request):
+	return web.FileResponse(f"html/icon-{request.match_info['type']}.svg")
+
 
 async def scooters(request):
 	return web.json_response(get_all_scooters())
@@ -12,4 +15,5 @@ async def scooters(request):
 app = web.Application()
 app.add_routes([web.get('/', index)])
 app.add_routes([web.get('/scooters.json', scooters)])
+app.add_routes([web.get('/marker-{type}.svg', marker)])
 web.run_app(app, port=5000)
