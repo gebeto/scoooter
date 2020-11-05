dafc3860db6e3c30e096c9254127530472fea436
gebeto <slavik.nychkalo@gmail.com>
2020-11-05 14:52:57 +0200

    Added credentials get from environment variables

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import os
+
+
+def credentials_from_env(name):
+    return os.environ.get(f"CREDENTIALS_{name.upper()}")
+
+
 def read_credentials(name):
+    env_credentials = credentials_from_env(name)
+    if env_credentials:
+        return env_credentials
     try:
         return open(f".credentials-{name}", "r").read()
     except FileNotFoundError:
